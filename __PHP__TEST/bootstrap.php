453b0195f64ad4972a593342efd8326bc35bf154
Sanjay Adhikari <culblank.minded@gmail.com>
2024-06-03 11:16:11 +1000

    Plugin urls

--- __PHP__TEST/bootstrap.php
+++ __PHP__TEST/bootstrap.php
@@ -13,4 +13,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Bootstrap WP_Mock to initialize built-in features.
 WP_Mock::bootstrap();
 
+WP_Mock::userFunction(
+	'plugin_dir_path',
+	[
+		'return' => __DIR__ . '/',
+	]
+);
+
+WP_Mock::userFunction(
+	'plugin_dir_url',
+	[
+		'return' => '/wp-content/plugins/wp-react-plugin-skeleton/',
+	]
+);
+
 require_once __DIR__ . '/../wp-react-plugin-skeleton.php';
